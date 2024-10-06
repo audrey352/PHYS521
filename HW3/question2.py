c59a0bda309601ff8a9b046d4025645b970fc548
audrey352 <aberniercan@gmail.com>
2024-10-06 19:22:36 -0400

    q3 and file organization

--- HW3/question2.py
+++ HW3/question2.py
@@ -4,7 +4,7 @@ import scipy.constants as const
 
 # Variables & Constants
 R = 1. / 91.17636  # Rydberg constant in nm^-1
-# sigma = const.Stefan_Boltzmann  # 5.67e-8 W/m^2/K^4
+wien_cst = 2.8977729e6  # Wien's constant in nm*K
 T_A0 = 9549.93  # K
 dir_path = '/Users/audrey/Documents/PHYS521/HW3/'
 
@@ -17,33 +17,50 @@ n_max = 20  # highest energy level to plot
 n = np.arange(3, n_max, 1)  # energy levels from 3 to high
 level2 = (1. / 2.) ** 2  # (1/2)^2
 leveln = (1. / n) ** 2  # (1/n)^2
-
 # Balmer formula
 invlam = R * (level2 - leveln)  # balmer formula, inverse wavelength nm-1
 lamda_balmer = 1. / invlam  # convert to wavelength nm
 
 
-
 # Figure
 fig = plt.figure(figsize=(10, 6), dpi=150)
-
 # Data
 for lam in lamda_balmer:
     label = 'Balmer Lines' if lam == lamda_balmer[0] else None
     plt.axvline(lam, color='darkorange', linewidth=0.8, label=label)
 plt.plot(lambda_A0,flux_A0/np.mean(flux_A0), linewidth=1.8, label='A0 Spectrum')  # Plot the normalized flux (using avg flux)
-
 # Labels & Axes
 plt.xlabel(r'$\lambda$ [nm]')
 plt.xlim(350,700)
 plt.ylabel(r'Flux$_{\lambda}$')
 plt.yscale('linear')
 plt.title(f'HW #3, Prob 2, Fig 1: Spectrum of the A0 Star with \nRydberg Balmer Formula up to n={n_max}', fontsize=15)
+# Plot
 plt.legend(loc='upper right')
-
 # Save
 plt.savefig(dir_path+f'q2_fig1.png')
-plt.show()
+# plt.show()
 
-# peak_predict = 
-# print(f'Predicted Location of Peak Temperature: {peak_predict} nm')
+
+# a) wavelength of peak intensity
+peak_predict = wien_cst/T_A0  # Wien's displacement law
+print(f'Predicted Location of Peak Intensity: {peak_predict} nm')
+
+# b)
+def emit_wavelength(n_start, n_end):
+    """
+    Calculate the wavelength associated with the transition between two energy levels.
+    """
+    inv_lam = R * (1/n_start**2 - 1/n_end**2)  # nm^-1
+    return np.abs(1/inv_lam)  # in nm
+
+lam_ionize = emit_wavelength(2, 1e9)  # nm, approximating infinity as 1e9
+print(f'Wavelength of the photon needed to ionize the hydrogen atom: {lam_ionize} nm')
+plt.axvline(lam_ionize, color='r', linestyle='--', label='Ionization Wavelength')  # add line to plot
+
+# Plot
+plt.title(f'HW #3, Prob 2, Fig 2: Spectrum of the A0 Star with \nRydberg Balmer Formula up to n={n_max}', fontsize=15)
+plt.legend(loc='upper right')
+# Save
+plt.savefig(dir_path+f'q2_fig2.png')
+plt.show()
---
+++ HW3/question3.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.constants as const
+
+# Constants
+k = const.k  # Boltzmann constant in J/K
+dir_path = '/Users/audrey/Documents/PHYS521/HW3/'
+
+
+# Functions
+def binding_energy(n):
+    return -13.6/n**2 * const.e  # in J
+
+def get_g(n):
+    """statistical weights that specify the number of sublevels in each n energy level
+    """
+    # g_n = 2n^2
+    return 2 * n**2
+
+def boltz_excite(ni, nj, Tk, k=k):
+    """Calculate the ratio of the number of atoms of the same element 
+    with electrons in any two energy levels, i (lower) and j (upper)
+    """
+    # Statistical weights
+    gi = get_g(ni)
+    gj = get_g(nj)
+    # Binding energy
+    Ei = binding_energy(ni)
+    Ej = binding_energy(nj)
+    # Ratio nj/ni
+    return gj/gi * np.exp(-(Ej - Ei)/(k*Tk))
+
+def plot_ratio(ni, nj, Tk, k=k):
+    """Plot the ratio nj/ni for a range of temperatures Tk
+    """
+    # Boltzmann ratio
+    ratio = boltz_excite(ni, nj, Tk, k)
+    # Plot
+    plt.figure(dpi=150)
+    plt.plot(Tk, ratio)
+    plt.xlabel('Temperature [K]')
+    plt.ylabel(r'$n_j/n_i$')
+    plt.title(f'Ratio of the number of atoms for a 2-level \nhydrogen atom, with n={ni} and n={nj}')
+    plt.savefig(dir_path+f'q3_boltz_ratio_{ni}_{nj}_{Tk[-1]:.0e}.png')
+    plt.show()
+
+    return ratio
+
+# a)
+ni = 1
+nj = 2
+T = np.linspace(2000, 1e5, 10000)
+nj_ni_ratio = plot_ratio(ni, nj, T)
+
+# c)
+index_eq = np.argmin(np.abs(nj_ni_ratio-1))
+T_eq = T[index_eq]
+print(f'Temperature at which the ratio n_j/n_i = 1: {T_eq} K')
+
+# d)
+index_10_percent = np.argmin(np.abs(nj_ni_ratio-0.1))
+T_10_percent = T[index_10_percent]
+print(f'Temperature at which the ratio n_j/n_i = 0.1: {T_10_percent} K')
+
+# e)
+ni = 1
+nj = 2
+T = np.linspace(2000, 1e9, 10000)
+nj_ni_ratio = plot_ratio(ni, nj, T)
